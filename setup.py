28c29250aa19e217abe5385498343060b739aae4
zhexinwu <1025839251@qq.com>
2024-07-11 13:47:44 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="IPODataAnalysis",
+    version="0.1",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "scikit-learn",
+        "torch",
+        "openpyxl",
+        "ipywidgets",
+        "tqdm",
+    ]
+)
