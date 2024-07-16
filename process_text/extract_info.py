e6de028a2df74c66178a78d192594021733c8f13
zhexinwu <1025839251@qq.com>
2024-07-16 11:44:26 +0800

    Developing handling each Q&A

--- process_text/extract_info.py
+++ process_text/extract_info.py
@@ -7,6 +7,16 @@ from IPODataAnalysis.configs import PATTERNS
 
 
 def extract_content(doc: fitz.Document):
+    """
+    output:
+    {
+        "question": str,
+        "link": {
+            "kind": 1,
+            "page": int,
+        }
+    }
+    """
     content_entry_pattern = re.compile(PATTERNS["content_entry"])
     content_pages = []
     has_found_content = False
@@ -39,6 +49,91 @@ def extract_content(doc: fitz.Document):
                     "page": link.get("page", -1),
                 }
             })
+
+    return res
+
+
+def extract_q_and_a(doc: fitz.Document, content_res: list):
+    """
+    content_res is output of extract_content(.)
+    """
+    pass
+
+
+def process_ans(doc: fitz.Document, start_page: int, end_page: int, reply_str: str, buffer=100):
+    """
+    buffer: heuristic to include multiline subtitle
+
+    Returns
+    -------
+    [
+        {
+            "page": int,
+            "subtitle": str,
+        }...
+    ]
+    """
+    subtitle_pattern = re.compile(PATTERNS["subtitle"])
+    res = []
+
+    for i, page_idx in enumerate(range(start_page, end_page + 1)):
+        page_str = doc.load_page(page_idx).get_text()
+        if i == 0:
+            sep_idx = page_str.find(reply_str)
+            page_str = page_str[sep_idx + len(reply_str):]
+        matches = re.findall(subtitle_pattern, page_str)
+        for match in matches:
+            sep_idx = page_str.find(match)
+            res.append({
+                "page": page_idx,
+                "subtitle": page_str[sep_idx:sep_idx + buffer].strip(),
+            })
+
+    return res
+
+
+def process_q_and_a(doc: fitz.Document, start_page: int, end_page: int):
+    """
+    Returns
+    -------
+    {
+        "question_long": str,
+        "ans_collection": list[dict] (output of process_ans(.)),
+    }
+    """
+    # Find "回复：" pattern
+    reply_pattern = re.compile(PATTERNS["reply"])
+    reply_page_idx = -1
+    reply_str = None
+    for page_idx in range(start_page, end_page + 1):
+        page = doc.load_page(page_idx)
+        page_text = page.get_text()
+        matches = re.findall(reply_pattern, page_text)
+        if len(matches) > 0:
+            reply_page_idx = page_idx
+            reply_str = matches[0]
+            break
+    # print(repr(reply_str))
+    if reply_page_idx == -1:
+        raise IndexError("Cannot find '回复：' pattern!")
+
+    res = {}
+    # Store the question
+    q_str = ""
+    for page_idx in range(start_page, reply_page_idx):
+        page = doc.load_page(page_idx)
+        q_str += page.get_text()
+    page = doc.load_page(page_idx)
+    page_str = page.get_text()
+    sep_idx = page_str.find(reply_str)
+    q_str += page_str[:sep_idx]
+    res["question_long"] = q_str
+
+    # Store the reply
+    # Ignore the last page since there's no sufficient content to make a substantial subsection
+    # in order to prevent overlap with the next question
+    ans_list = process_ans(doc, reply_page_idx, end_page - 1, reply_str)
+    res["ans_collection"] = ans_list
 
     return res
 
